1e6c6a0c6fd151d74c5aefa9ecd762e111cfa407
Kendall Carter <kendallacarter1@gmail.com>
2023-05-12 20:56:54 -0500

    Initial Commit

---
+++ PracticePal/PracticePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticePalApp.swift
+//  PracticePal
+//
+//  Created by Kendall Carter on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
